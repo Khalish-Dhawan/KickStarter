067dc6cd4150e82c78b3e5f3f1effa9506cc3793
Khalish-Dhawan <74649247+Khalish-Dhawan@users.noreply.github.com>
2022-11-26 20:48:01 +0530

    Update factory.js

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,5 +3,5 @@ const web3 = require("./web3");
 
 module.exports = new web3.eth.Contract(
   campaignFactory.abi,
-  "0xC64Af0bB0f5Af77aA0c8Ea40fa9514c871A1d0bb"
+  "Deployed Address of factory obtained after deployment"
 );
