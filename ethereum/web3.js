e3749a7ef1ba9ff9812b57dce112a50ab37c2448
Khalish-Dhawan <74649247+Khalish-Dhawan@users.noreply.github.com>
2022-11-26 20:46:51 +0530

    Update web3.js

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,4 +1,5 @@
 const Web3 = require("web3");
+require("dotenv").config();
 
 let web3;
 
@@ -8,7 +9,7 @@ if (typeof window !== "undefined" && typeof window.web3 !== "undefined") {
 } else {
   // we are on the server OR the user is not running metamask
   const provider = new Web3.providers.HttpProvider(
-    "https://rinkeby.infura.io/v3/1d01e94d5f5447f0953b2d88afc9d9d7"
+    process.env.INFURA
   );
   web3 = new Web3(provider);
 }
